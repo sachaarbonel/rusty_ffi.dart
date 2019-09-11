6b94f22fd28d7d4492a1c36bea6d8e512c423208
Sach97 <sacha.arbonel@hotmail.fr>
2019-09-11 18:58:22 +0200

    more complex example : audio

--- bin/main.dart
+++ bin/main.dart
@@ -1,6 +1,16 @@
-import 'package:rusty_ffi/rusty_ffi.dart' as rust;
+import 'dart:ffi' as ffi;
+import 'dart:ffi';
+import 'package:ffi/ffi.dart';
+
+typedef NativeRustPlayOnceFunction = ffi.Void Function(ffi.Pointer<Utf8>);
+typedef NativePlayOnceFunction = void Function();
 
 main() {
-  var number = rust.add(12, 13);
-  print("call rust function add=$number");
+  ffi.DynamicLibrary dl =
+      ffi.DynamicLibrary.open("../target/debug/libplay_once.so");
+  var play_once =
+      dl.lookupFunction<NativeRustPlayOnceFunction, NativeRustPlayOnceFunction>(
+          "play_once");
+  final Pointer<Utf8> song = Utf8.toUtf8("beep.wav").cast();
+  play_once(song);
 }
