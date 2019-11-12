f2157b53be16ae00cdf3942f3416f07f2329308f
sacha <sacha.arbonel@hotmail.fr>
2019-11-12 11:54:46 +0100

    fix #1

--- bin/main.dart
+++ bin/main.dart
@@ -1,16 +1,30 @@
+// Import dart:ffi.
 import 'dart:ffi' as ffi;
-import 'dart:ffi';
+// Utilities for working with ffi like String
 import 'package:ffi/ffi.dart';
 
-typedef NativeRustPlayOnceFunction = ffi.Void Function(ffi.Pointer<Utf8>);
-typedef NativePlayOnceFunction = void Function();
+import 'dart:io' show Platform;
+
+// Create a typedef with the FFI type signature of the C function.
+// Commonly used types defined by dart:ffi library include Double, Int32, NativeFunction, Pointer, Struct, Uint8, and Void.
+typedef play_once_func = ffi.Void Function(ffi.Pointer<Utf8>);
+
+// Create a typedef for the variable that you’ll use when calling the C function.
+typedef PlayOnce = void Function(ffi.Pointer<Utf8>);
 
 main() {
-  ffi.DynamicLibrary dl =
-      ffi.DynamicLibrary.open("target/debug/libplay_once.so");
-  var play_once =
-      dl.lookupFunction<NativeRustPlayOnceFunction, NativeRustPlayOnceFunction>(
-          "play_once");
-  final Pointer<Utf8> song = Utf8.toUtf8("data/beep.wav").cast();
+  // Open the dynamic library that contains the C function.
+  ffi.DynamicLibrary dylib = ffi.DynamicLibrary.open(Platform.isMacOS
+      ? "target/debug/libplay_once.dylib"
+      : "target/debug/libplay_once.so");
+  // Get a reference to the C function, and put it into a variable. This code uses the typedefs defined in steps 2 and 3, along with the dynamic library variable from step 4.
+  final PlayOnce play_once = dylib
+      .lookup<ffi.NativeFunction<play_once_func>>('play_once')
+      .asFunction();
+
+  // Convert a Dart [String] to a Utf8-encoded null-terminated C string.
+  final ffi.Pointer<Utf8> song = Utf8.toUtf8("data/beep2.wav").cast();
+
+  // Call the C function.
   play_once(song);
 }
